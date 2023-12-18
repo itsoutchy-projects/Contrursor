ca62a72ded0735e7cc1a084ee7f1fefc7c6fd2fc
itsoutchy <itsoutchy55@yahoo.com>
2023-12-18 22:12:18 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Hardcodet.Wpf.TaskbarNotification;
+using Hardcodet.Wpf.TaskbarNotification.Interop;
+using System.Windows.Forms;
+
+namespace Contrursor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller controller = new Controller(UserIndex.One);
+
+        public bool sentUpEvent = true;
+        public bool sentDownEvent = false;
+
+        public bool rightSentUpEvent = true;
+        public bool rightSentDownEvent = false;
+
+        public int sensitivity = 10;
+
+        public bool sentStartKeyEvent = false;
+
+        public int scrollspeed = 50;
+
+        
+        static bool upDown = false;
+        static bool upUp = true;
+
+        static bool downDown = false;
+        static bool downUp = true;
+
+        static bool leftDown = false;
+        static bool leftUp = true;
+
+        static bool rightDown = false;
+        static bool rightUp = true;
+
+        //NotifyIconData iconData = new NotifyIconData();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //iconData.BalloonTitle = "Contrursor";
+            DispatcherTimer timer = new DispatcherTimer();
+            //NotifyIcon thisicon = new NotifyIcon();
+            //BitmapImage bitmapImage = new BitmapImage();
+            //thisicon.Icon = new System.Drawing.Icon("pack://application:,,,/icon.ico");
+            //thisicon.Text = "Contrursor";
+            //thisicon.ShowBalloonTip(0);
+            timer.Interval = new TimeSpan(10000);
+            timer.Tick += GamePadStuff;
+            timer.Start();
+        }
+
+        public void GamePadStuff(object? sender, EventArgs e)
+        {
+            if (controller.IsConnected)
+            {
+                Gamepad gamepad = controller.GetState().Gamepad;
+
+                //Axis 
+                //LeftThumbX, LeftThumbY, RightThumbX, RightThumbY
+
+                //these are short so to get a float from 0 to 1 instead :
+                short gx = gamepad.LeftThumbX;
+                float tx = (gamepad.LeftThumbX / (float)short.MaxValue) * sensitivity;
+                Simulator.MouseX += (int)tx;
+
+                short gy = gamepad.LeftThumbY;
+                float ty = (gamepad.LeftThumbY / (float)short.MaxValue) * sensitivity;
+                Simulator.MouseY -= (int)ty;
+
+                //Buttons
+                GamepadButtonFlags Buttons = gamepad.Buttons;
+                if (Buttons.HasFlag(GamepadButtonFlags.Start) && !sentStartKeyEvent)
+                {
+                    Simulator.PressKey(0x5B);
+                    sentStartKeyEvent = true;
+                } else
+                {
+                    sentStartKeyEvent = false;
+                }
+                if ((Buttons.HasFlag(GamepadButtonFlags.A) || gamepad.LeftTrigger > 0.5f) && !sentDownEvent)
+                {
+                    Simulator.MouseEvent(Simulator.MouseEventFlags.LEFTDOWN, 0, 0, Simulator.MouseEventDataXButtons.XBUTTON1);
+                    sentUpEvent = false;
+                    sentDownEvent = true;
+                } else if (sentUpEvent == false && !Buttons.HasFlag(GamepadButtonFlags.A) && gamepad.LeftTrigger < 0.5f)
+                {
+                    Simulator.MouseEvent(Simulator.MouseEventFlags.LEFTUP, 0, 0, Simulator.MouseEventDataXButtons.XBUTTON1);
+                    sentUpEvent = true;
+                    sentDownEvent = false;
+                }
+
+                if (Buttons.HasFlag(GamepadButtonFlags.RightShoulder))
+                {
+                    Simulator.MouseEvent(Simulator.MouseEventFlags.WHEEL, 0, 0, scrollspeed);
+                }
+
+                if (Buttons.HasFlag(GamepadButtonFlags.LeftShoulder))
+                {
+                    Simulator.MouseEvent(Simulator.MouseEventFlags.WHEEL, 0, 0, -scrollspeed);
+                }
+
+                if (gamepad.RightTrigger > 0.5f && !rightSentDownEvent)
+                {
+                    Simulator.MouseEvent(Simulator.MouseEventFlags.RIGHTDOWN, 0, 0, Simulator.MouseEventDataXButtons.XBUTTON1);
+                    rightSentUpEvent = false;
+                    rightSentDownEvent = true;
+                }
+                else if (rightSentUpEvent == false && gamepad.RightTrigger < 0.5f)
+                {
+                    Simulator.MouseEvent(Simulator.MouseEventFlags.RIGHTUP, 0, 0, Simulator.MouseEventDataXButtons.XBUTTON1);
+                    rightSentUpEvent = true;
+                    rightSentDownEvent = false;
+                }
+
+                #region DPad -> arrow keys
+                if (Buttons.HasFlag(GamepadButtonFlags.DPadUp) && !upDown)
+                {
+                    Simulator.keyDown(0x26);
+                    upDown = true;
+                    upUp = false;
+                } else if (!Buttons.HasFlag(GamepadButtonFlags.DPadUp) && !upUp)
+                {
+                    Simulator.keyUp(0x26);
+                    upDown = false;
+                    upUp = true;
+                }
+
+                if (Buttons.HasFlag(GamepadButtonFlags.DPadDown) && !downDown)
+                {
+                    Simulator.keyDown(0x28);
+                    downDown = true;
+                    downUp = false;
+                }
+                else if (!Buttons.HasFlag(GamepadButtonFlags.DPadDown) && !downUp)
+                {
+                    Simulator.keyUp(0x28);
+                    downDown = false;
+                    downUp = true;
+                }
+
+                if (Buttons.HasFlag(GamepadButtonFlags.DPadLeft) && !leftDown)
+                {
+                    Simulator.keyDown(0x25);
+                    leftDown = true;
+                    leftUp = false;
+                }
+                else if (!Buttons.HasFlag(GamepadButtonFlags.DPadLeft) && !leftUp)
+                {
+                    Simulator.keyUp(0x25);
+                    leftDown = false;
+                    leftUp = true;
+                }
+
+                if (Buttons.HasFlag(GamepadButtonFlags.DPadRight) && !rightDown)
+                {
+                    Simulator.keyDown(0x27);
+                    rightDown = true;
+                    rightUp = false;
+                }
+                else if (!Buttons.HasFlag(GamepadButtonFlags.DPadRight) && !rightUp)
+                {
+                    Simulator.keyUp(0x27);
+                    rightDown = false;
+                    rightUp = true;
+                }
+                #endregion
+            }
+        }
+    }
+}
---
+++ Simulator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contrursor
+{
+    public static class Simulator
+    {
+        #region Mouse
+        [StructLayout(LayoutKind.Sequential)]
+        struct POINT
+        {
+            public Int32 X;
+            public Int32 Y;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT point);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+
+        //Use the values of this enum for the 'dwData' parameter
+        //to specify an X button when using MouseEventFlags.XDOWN or
+        //MouseEventFlags.XUP for the dwFlags parameter.
+        public enum MouseEventDataXButtons : uint
+        {
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+
+
+        public static int MouseX
+        {
+            get
+            {
+                POINT pos = new POINT();
+                GetCursorPos(out pos);
+                return pos.X;
+            }
+            set
+            {
+                POINT pos = new POINT();
+                GetCursorPos(out pos);
+                SetCursorPos(value, pos.Y);
+            }
+        }
+
+        public static int MouseY
+        {
+            get
+            {
+                POINT pos = new POINT();
+                GetCursorPos(out pos);
+                return pos.Y;
+            }
+            set
+            {
+                POINT pos = new POINT();
+                GetCursorPos(out pos);
+                SetCursorPos(pos.X, value);
+            }
+        }
+
+        public static void MouseEvent(MouseEventFlags flags, int x, int y, MouseEventDataXButtons XButtons, int extraInfo = 0)
+        {
+            mouse_event((uint)flags, x, y, (uint)XButtons, extraInfo);
+        }
+
+        public static void MouseEvent(MouseEventFlags flags, int x, int y, uint XButtons, int extraInfo = 0)
+        {
+            mouse_event((uint)flags, x, y, XButtons, extraInfo);
+        }
+
+        public static void MouseEvent(MouseEventFlags flags, int x, int y, int XButtons, int extraInfo = 0)
+        {
+            mouse_event((uint)flags, x, y, (uint)XButtons, extraInfo);
+        }
+        #endregion
+
+        #region Keys
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        private const int KEYEVENTF_EXTENDEDKEY = 0x1;
+        private const int KEYEVENTF_KEYUP = 0x2;
+
+        public static void PressKey(byte keyCode)
+        {
+            keybd_event(keyCode, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event(keyCode, 0x45, KEYEVENTF_KEYUP, 0);
+        }
+
+        public static void keyDown(byte keyCode)
+        {
+            keybd_event(keyCode, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+        }
+
+        public static void keyUp(byte keyCode)
+        {
+            keybd_event(keyCode, 0x45, KEYEVENTF_KEYUP, 0);
+        }
+        #endregion
+    }
+}
